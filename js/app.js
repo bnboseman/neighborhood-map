b9ee0a62ba24a3311939120c632989d746360a6a
Nichole Boseman <bnboseman@gmail.com>
2015-11-23 21:32:35 -0500

    working on adding yelp search

--- js/app.js
+++ js/app.js
@@ -1,33 +1,65 @@
 (function() {
+        function yelp(searchNear, searchFor) {
+        var auth = { 
+                consumerKey: "oZsD8h9BM0VQtveN7sYvHg", 
+                consumerSecret: "EQhmDrmyjsJH7F5s3SEe516JbGY",
+                accessToken: "N-R-C5cfJXo-4LrgGcpBJUQ6w3Y2_I6_",
+                accessTokenSecret: "H9H8IFqCUxHqXxqJuoM0dEtHiDo",
+                serviceProvider : {
+			        signatureMethod : "HMAC-SHA1"
+			    }
+        };
         
+        var accessor = {
+                consumerSecret : auth.consumerSecret,
+                tokenSecret : auth.accessTokenSecret
+        };
+        
+
+        var parameters = [];
+        parameters.push(['term', searchFor]);
+        parameters.push(['location', searchNear]);
+        parameters.push(['callback', 'cb']);
+        parameters.push(['oauth_consumer_key', auth.consumerKey]);
+        parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+        parameters.push(['oauth_token', auth.accessToken]);
+        parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+        
+        
+        var message = {
+                'action' : 'http://api.yelp.com/v2/search',
+                'method' : 'GET',
+                'parameters' : parameters
+        };
+        
+        OAuth.setTimestampAndNonce(message);
+        OAuth.SignatureMethod.sign(message, accessor);
+
+        var parameterMap = OAuth.getParameterMap(message.parameters);
+        parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+      
+        $.ajax({
+                'url' : message.action,
+                'data' : parameterMap,
+                'dataType' : 'jsonp',
+                'global' : true,
+                'jsonpCallback' : 'cb',
+                'success' : function(data){
+                        console.log(data);
+                }
+        });
+        }
         var MapViewModel = function() {
                 var self = this;
                 
                 // Google map to display NYC
-                self.map =  new google.maps.Map(document.getElementById('map'), {
+                this.map =  new google.maps.Map(document.getElementById('map'), {
                         center: {lat: 40.7577, lng: -73.9857},
                         zoom: 14
                 });
                 
                 this.markers = new ko.observableArray();
                 this.searchFilter =  ko.observable('');
-                
-                this.coordinates = [
-                        new self.createLocation('Guitar Center', 40.757, -73.987),
-                        new self.createLocation('Guitar Center', 40.736817, -73.994547),
-                        new self.createLocation('Jazz Standard', 40.742158, -73.983826),
-                        new self.createLocation('Decade', 40.761092, -73.960916),
-                        new self.createLocation('Iridium', 40.761816, -73.983389),
-                        new self.createLocation('Jazz Gallery', 40.744605, -73.988547),
-                        new self.createLocation('Guitar New York', 40.762517, -73.977761),
-                        new self.createLocation('Metropolitan Room', 40.741462, -73.992067),
-                        new self.createLocation('Sam Ash', 40.753, -73.994 ),
-                        new self.createLocation("Rudy's Music (Closed)", 40.759, -73.983),
-                        new self.createLocation('Birdland Jazz Club', 40.759194, -73.989800),
-                        new self.createLocation('Colony Music (Closed)', 40.761061, -73.984740),
-                        new self.createLocation('Museum of Modern Art', 40.761417, -73.977120),
-                        new self.createLocation("Dizzy's Jazz Club", 40.768425, -73.982)
-                ];
                 
                 /** Funtion to create a locations  for markers array
                  * @param title string the name of the location
@@ -36,7 +68,7 @@
                  * @param detail string TODO the information for the infoWindow
                  * @return an object of the location added
                  */ 
-                this.createLocation = function( title, latitude, longitude, data ) {
+                this.createLocation      = function( title, latitude, longitude, data ) {
                         var location = {
                                 position: new google.maps.LatLng(latitude, longitude),
                                 title:title,
@@ -56,12 +88,31 @@
                     return location;
                 };
                 
+                this.coordinates = [
+                        new self.createLocation('Guitar Center', 40.757, -73.987),
+                        new self.createLocation('Guitar Center', 40.736817, -73.994547),
+                        new self.createLocation('Jazz Standard', 40.742158, -73.983826),
+                        new self.createLocation('Decade', 40.761092, -73.960916),
+                        new self.createLocation('Iridium', 40.761816, -73.983389),
+                        new self.createLocation('Jazz Gallery', 40.744605, -73.988547),
+                        new self.createLocation('Guitar New York', 40.762517, -73.977761),
+                        new self.createLocation('Metropolitan Room', 40.741462, -73.992067),
+                        new self.createLocation('Sam Ash', 40.753, -73.994 ),
+                        new self.createLocation("Rudy's Music (Closed)", 40.759, -73.983),
+                        new self.createLocation('Birdland Jazz Club', 40.759194, -73.989800),
+                        new self.createLocation('Colony Music (Closed)', 40.761061, -73.984740),
+                        new self.createLocation('Museum of Modern Art', 40.761417, -73.977120),
+                        new self.createLocation("Dizzy's Jazz Club", 40.768425, -73.982)
+                ];
+                
+                
+                
                 /**
                  * Function to handle input from search field. Automatically updates the locations based on search text
                  * The function updates the sidelist and the visible markers based up what the user wishes to filter
                  * @param searchValue the newly input text to the search field
                  */
-                self.searchFilter.subscribe(function(searchValue) {
+                this.searchFilter.subscribe(function(searchValue) {
                         searchValue = searchValue.toLowerCase();
                         var change = false;
                         ko.utils.arrayForEach( self.markers(), function(marker) {
